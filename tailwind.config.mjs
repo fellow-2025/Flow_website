64325582df84a38a3216506f54aadb6d39e50133
sbm <ex@example.com>
2024-11-13 15:46:04 +0900

    added base contents

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				shan: ['Source Sans Pro', 'sans-serif'],
+				nijimi: ['MaboroshiNoNijimi', 'serif']
+			}
+		},
 	},
 	plugins: [],
 }
