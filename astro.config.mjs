893c1e6c31856f1f5899fd2e284a9db168d22562
sbm <g.sakurai123@gmail.com>
2025-02-24 00:35:43 +0900

    cloudflare integration (i forgot merge changes to main on last build)

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,11 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: netlify(),
+  adapter: cloudflare(),
   integrations: [tailwind(), react()]
 });
