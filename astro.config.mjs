f24906674495a250ae42b6c077cfc296a2baad8b
sbm <g.sakurai123@gmail.com>
2025-02-24 00:38:09 +0900

    cloudflare react workaround

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,5 +13,14 @@ import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
   output: 'server',
   adapter: cloudflare(),
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  vite: {
+    resolve: {
+      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+      alias: import.meta.env.PROD ? {
+        "react-dom/server": "react-dom/server.edge",
+      } : undefined,
+    },
+  },
 });
