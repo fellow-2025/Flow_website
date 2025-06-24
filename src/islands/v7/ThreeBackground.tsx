993c8b0bf98b6bf21e55d0226a6f2d683570c042
sbm <g.sakurai123@gmail.com>
2025-06-25 00:58:49 +0900

    three.js background base

---
+++ src/islands/v7/ThreeBackground.tsx
@@ -0,0 +1,78 @@
+import * as React from 'react'
+import * as THR from 'three'
+
+const initialize = (rndr: THR.WebGLRenderer, cam: THR.OrthographicCamera) => {
+    const vv = window.visualViewport
+
+    const w = vv ? vv.width : window.innerWidth
+    const h = vv ? vv.height : window.innerHeight
+
+    rndr.setSize(w, h)
+
+    cam.left =      -w/2
+    cam.right =      w/2
+    cam.top =       -h/2
+    cam.bottom =     h/2
+    cam.updateProjectionMatrix()
+}
+
+export default () => {
+    // what is this
+    const canvRef = React.useRef<HTMLCanvasElement | null>(null)
+
+    // what is this II
+    React.useEffect(() => {
+        if(!canvRef.current) return
+
+
+        // RENDERER & CAMERA SETUP
+
+        const rndr = new THR.WebGLRenderer({
+            canvas: canvRef.current,
+            alpha: true
+        })
+        
+        
+        // create camera with temporal aspect ratio
+        const cam = new THR.OrthographicCamera()
+
+
+        // fit size
+        let resizeTimeout = NaN
+
+        {
+            const vv = window.visualViewport
+            if (vv){
+                vv.addEventListener("resize", _ => initialize(rndr, cam))
+            }else{
+                window.addEventListener('resize', _ => initialize(rndr, cam))
+            }
+        }
+        
+        initialize(rndr, cam)
+        
+
+        // ADD OBJECTS
+
+        const scn = new THR.Scene()
+
+
+        // RENDER LOOP
+
+        let fr = 0
+        const tick = () => {
+            fr++
+
+            requestAnimationFrame(tick)
+        }
+
+        tick()
+
+        // Xx_what is this_xX ("resource release on component unload" by chatGPT)
+        return () => rndr.dispose()
+    })
+
+    return (
+        <canvas id='threeCanv' ref={canvRef} className='fixed -z-10'></canvas>
+    )
+}
